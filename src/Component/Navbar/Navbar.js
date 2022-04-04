8fc60faa0254c7ec9087d523401094d4e3910a44
rijonshahariar <shahariar.rijon@gmail.com>
2022-04-04 19:57:03 +0600

    navigation bar added

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <NavLink
+                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                to="/"
+            >
+                HOME
+            </NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                to="/reviews"
+            >
+                REVIEWS
+            </NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                to="/dashboard"
+            >
+                DASHBOARD
+            </NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                to="/blogs"
+            >
+                BLOGS
+            </NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                to="/videos"
+            >
+                ABOUT
+            </NavLink>
+
+        </div>
+    );
+};
+
+export default Navbar;
